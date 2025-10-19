469d242837c2ca07361cd3131cf0e1c0cc6c1d87
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-10-19 13:35:58 +0200

    added copyright and timestamp comments

--- src-prog-maj/uPlanning.pas
+++ src-prog-maj/uPlanning.pas
@@ -1,3 +1,35 @@
+﻿(* C2PP
+  ***************************************************************************
+
+  Planning API
+
+  Copyright 2022-2025 Patrick PREMARTIN under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://planningapi.olfsoftware.fr
+
+  Project site :
+  https://github.com/DeveloppeurPascal/Planning-API
+
+  ***************************************************************************
+  File last update : 2025-10-16T10:42:16.772+02:00
+  Signature : 1713f3591ccfad1f5f6f5af4252ee51ef29fcc94
+  ***************************************************************************
+*)
+
 unit uPlanning;
 
 interface
